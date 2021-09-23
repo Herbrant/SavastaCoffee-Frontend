9cd4907258bc32664bb7c0794c448390aed0871f
Herbrant <cdavide98carnemolla@gmail.com>
2021-09-23 11:51:51 +0200

    feat: added login page

---
+++ src/views/Login.vue
@@ -0,0 +1,24 @@
+<template>
+	<b-container>
+		<b-row class="justify-content-center">
+			<h3>Sign In</h3>
+			<b-col cols="sm-4">
+				<label>Email address</label>
+				<input type="email" class="form-control form-control-lg" />
+			
+				<div class="form-group">
+					<label>Password</label>
+					<input type="password" class="form-control form-control-lg" />
+				</div>
+				
+				<button type="submit" class="btn btn-dark btn-lg btn-block my-2">Sign In</button>
+			</b-col>
+		</b-row>
+	</b-container>
+</template>
+
+<script>
+    export default {
+		name: "Login"
+    }
+</script>
